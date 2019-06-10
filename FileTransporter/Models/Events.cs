6bc211ea712566c1306f00ea7de3c7f9f286476d
Mateusz Okrój <mateuszokroj1@hotmail.com>
2019-06-10 16:33:00 +0200

    Created projects of code library, server and client for Windows.

---
+++ FileTransporter/Models/Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FileTransporter.Models
+{
+    #region Delegates
+
+    public delegate void ServerEventHandler(object sender, ServerEventArgs e);
+
+    #endregion
+
+    #region Types
+
+    public class ServerEventArgs : EventArgs
+    {
+        public Server Server { get; set; }
+    }
+
+    #endregion
+}
